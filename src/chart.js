82e464ce1edab557083ad93c39eb228327e68632
robusticus <wtd.dev@gmail.com>
2026-04-15 06:22:05 -0700

    eslint Fix 3

--- src/chart.js
+++ src/chart.js
@@ -118,11 +118,11 @@ function Chart() {
     // after fetch
     useEffect(() => {
         if (chartData.length > 0) {
-            // TODO: The obvious changes here cause the state to close out and be inaccessible
-            // This may be that rare case where a component should be a class, not a function
-            // eslint-disable-next-line
-            StartStop(); // eslint-disable-line
+            StartStop(); 
         }
+    // TODO: The obvious changes here cause the state to close out and be inaccessible
+    // This may be that rare case where a component should be a class, not a function
+    // eslint-disable-next-line
     }, [chartData]);
     
     // Timer switch for button and init
